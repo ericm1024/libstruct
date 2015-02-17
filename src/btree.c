9d93a0e40d2097254f0231ce400007125336a575
Eric Mueller <eric.mueller1024@gmail.com>
2015-02-17 15:59:08 -0800

    Wrote a node allocator.
    Changed node->class to a bitfield instead of an enum to allow for
    nodes that are both root and leaf (i.e. the first node in a tree).
    
    Wrote a helper function to perform binary search through the keys
    in a node and find the index of the child node containing a given
    key.
    
    Added high-level pesudocode for insert.

--- src/btree.c
+++ src/btree.c
@@ -29,9 +29,19 @@
 /*! Number of child pointers in each btree node */
 #define BT_NUM_CHILDREN (BT_ORDER * 2 + 1)
 
+/* node class */
+#define ROOT_NODE (0x1u)
+#define BODY_NODE (0x2u)
+#define LEAF_NODE (0x4u)
+
+/* error conditions */
+#define BT_EEXISTS (-1)
+#define BT_ENOMEM (-2)
+
 /*! internal btree node. */
 struct bt_node {
-        int is_leaf; /*! 1 if the node is a leaf, 0 if it is a body node */
+        uint32_t class;
+	unsigned int nkeys; /*! number of keys currently in the node */
         uint64_t keys[BT_NUM_KEYS]; /*! keys seperating children !*/
         union {  
                 const void *leaves[BT_NUM_KEYS]; 
@@ -39,18 +49,70 @@ struct bt_node {
                 struct bt_node *children[BT_NUM_CHILDREN];
                              /*! child nodes, if the node is a body node */
         };
-        struct bt_node *parent;
+	union {
+		struct bt_node *parent; /*! parent of the node */
+		struct btree_t *head; /*! head of the tree (if root node) */
+	};
 };
 typedef struct bt_node node_t;
 
-static node_t *bt_alloc_node()
+/*
+ * Allocate a new node an initialize it with the given class.
+ */ 
+static node_t *bt_alloc_node(uint32_t class)
 {
-        return NULL;
+	node_t *n = (node_t *)malloc(sizeof(node_t));
+	if (n) {
+		n->class = class;
+		n->nkeys = 0;
+		n->parent = NULL;
+	}
+        return n;
+}
+
+/* shift all keys and leaves/children starting at
+ * index and offseting everything by shift */
+static void bt_shift_leaves(node_t *n, size_t index, int shift)
+{
+	
+}
+
+/*
+ * Find the index of the child node containing key. Binary search.
+ * Returns BT_EEXISTS if the key already exists
+ */ 
+static int bt_binsearch_node(node_t *n, uint64_t key)
+{
+	int win_s = -1; /* start of search window */
+	int win_e = n->nkeys; /* end of search window */
+	int i; /* index under consideration */
+	do {
+		/* compute i */
+		i = win_s + (win_e - win_s)/2;
+
+		/* termination conditions */
+		if (n->keys[i] == key)
+			return BT_EEXISTS;
+		else if (win_s - win_e == 1)
+			return win_e;
+
+		/* recompute win_s and win_e */
+		if (n->keys[i] < key)
+			win_s = i;
+		else
+			win_e = i;
+	} while (1);
 }
 
 int btree_init(btree_t *bt)
 {
-        return 1;
+	node_t *n = bt_alloc_node(ROOT_NODE | LEAF_NODE);
+	if (!n) {
+		return BT_ENOMEM;
+	}
+	n->head = bt;
+	bt->root = n;
+        return 0;
 }
 
 void btree_destroy(btree_t *bt)
@@ -60,6 +122,18 @@ void btree_destroy(btree_t *bt)
 
 int btree_insert(btree_t *bt, uint64_t key, const void *value)
 {
+	/* -- empty tree case --
+	 * Allocate a new node, initialize it as both a root and leaf node,
+	 * then use common co
+	 */
+
+	/* -- traverse to leaf --
+	 * 
+         */
+
+	/* insert into leaf */
+
+	/* traverse back up and rebalance */
         return 1;
 }
 
