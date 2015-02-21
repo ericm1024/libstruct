fb2a9cd31bbc21fa0a43d89b3939270fd6b432d5
Eric Mueller <eric.mueller1024@gmail.com>
2015-02-21 08:21:42 -0800

    Added licenses to some files I forgot to add licenses to previously.

--- src/flist.c
+++ src/flist.c
@@ -1,8 +1,24 @@
-/* Eric Mueller
- * flist.c
- * December 2014
+/*
+ * Copyright 2014 Eric Mueller
  *
- * Implementation of a forward list (singly linked).
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+/**
+ * \file flist.c
+ * 
+ * \author Eric Mueller
+ * 
+ * \brief Implementation of a forward list (singly linked).
  */
 
 #include "flist.h"
@@ -120,3 +136,4 @@ void flist_for_each_range(struct flist_head *hd, void (*f)(void *data),
 	  i = next;
   }
 }
+
